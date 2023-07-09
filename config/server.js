cdc7bc383964ccbe222ad1247345a94c2be39806
Samy Bahi <samy.bahi.business@gmail.com>
2023-07-09 19:58:09 +0200

    updated config server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "http://api.samybahi.com/test",
   app: {
     keys: env.array("APP_KEYS"),
   },
